205e36b238c2f510c1336a84e68d2c65ecfe5b97
oabuasba <156145936+oabuasba@users.noreply.github.com>
2024-09-28 14:37:53 -0700

    added tables

--- server/src/db.py
+++ server/src/db.py
@@ -16,7 +16,43 @@ def create_tables(db_cur: sqlite3.Cursor):
                          
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY,
-            username TEXT NOT NULL UNIQUE,
-            hashed_password TEXT NOT NULL
+            username TEXT NOT NULL,
+            hashed_password TEXT NOT NULL,
+            first_name TEXT,
+            last_name TEXT,
+            gym_loc TEXT,
+            gender TEXT,
+            phone TEXT
+        );
+        
+        CREATE TABLE IF NOT EXISTS workout (
+            workoutId INTEGER PRIMARY KEY,
+            name TEXT,
+            time TEXT,
+            tag1 TEXT,
+            tag2 TEXT,
+            tag3 TEXT,
+            desc TEXT,
+            ownerId INTEGER,
+            FOREIGN KEY (ownerId) REFERENCES users(id)
+        );
+
+        CREATE TABLE IF NOT EXISTS workout_members (
+            workout_id INTEGER,
+            user_id INTEGER,
+            PRIMARY KEY (workout_id, user_id),
+            FOREIGN KEY (workout_id) REFERENCES workout(workoutId),
+            FOREIGN KEY (user_id) REFERENCES user(id)
+        );
+
+        CREATE TABLE IF NOT EXISTS requests (
+            ownerId INTEGER,
+            workout INTEGER,
+            participant INTEGER,
+            accepted INTEGER
+            PRIMARY KEY (ownerId, workout, participant)
+            FOREIGN KEY (ownerId) REFERENCES workout(ownerId)
+            FOREIGN KEY (workout) REFERENCES workout(workoutId)
+            FOREIGN KEY (participant) REFERENCES user(id)
         );
     """)
